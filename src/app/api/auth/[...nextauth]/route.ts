5bfba5d7cfe951f0aa2e60a1bed352ee47f8b03c
aasuyadav1 <aasuy284@gmail.com>
2024-06-14 17:03:50 +0530

    added- github auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,44 +1,51 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import GithubProvider from 'next-auth/providers/github';
 import { dbConnect } from '@/lib/dbConnect';
 import User from '@/Models/userModel';
 
- const authOptions : any =  {
+const authOptions: any = {
   providers: [
     GoogleProvider({
       clientId: process.env.AUTH_GOOGLE_ID as string,
       clientSecret: process.env.AUTH_GOOGLE_SECRET as string,
     }),
+    GithubProvider({
+      clientId: process.env.AUTH_GITHUB_ID as string,
+      clientSecret: process.env.AUTH_GITHUB_SECRET as string,
+    }),
   ],
   callbacks: {
-    async session({ session } : any) {
-      // store the user id from MongoDB to session
+    async session({ session }: any) {
+      // Store the user id from MongoDB to session
       const sessionUser = await User.findOne({ email: session.user.email });
-    
-       if(sessionUser) {
-         session.user.id = sessionUser._id;
-       }
-      
+
+      if (sessionUser) {
+        session.user.id = sessionUser._id;
+      }
 
       return session;
     },
-    async signIn( {user , account }: any) {
-      if (account?.provider === 'google') {
-        const { name, email, image } = user;
-        try {
-          await dbConnect();
+    async signIn({ user, account }: any) {
+      const { name, email, image } = user;
+
+      try {
+        await dbConnect();
+
+        // Check if the account provider is Google or GitHub
+        if (account?.provider === 'google' || account?.provider === 'github') {
           const existingUser = await User.findOne({ email });
-  
+
           if (!existingUser) {
             await User.create({ name, email, image });
           }
-          return true;
-        } catch (error) {
-          console.error(error);
-          return false;
         }
+
+        return true;
+      } catch (error) {
+        console.error(error);
+        return false;
       }
-      return true;
     },
   },
   secret: process.env.NEXTAUTH_SECRET,
@@ -47,5 +54,3 @@ import User from '@/Models/userModel';
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
-
-
