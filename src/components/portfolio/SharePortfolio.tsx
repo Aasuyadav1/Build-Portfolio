c659639845bb9d15c023f08a5bb5f6df19cc9865
aasuyadav1 <aasuy284@gmail.com>
2024-06-16 10:57:16 +0530

    fix- ui

--- src/components/portfolio/SharePortfolio.tsx
+++ src/components/portfolio/SharePortfolio.tsx
@@ -1,10 +1,21 @@
-'use client'
-
+"use client";
 import React, { useEffect } from "react";
 import Link from "next/link";
-
-
-
+import { CopyIcon } from "@radix-ui/react-icons";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import {
   FacebookShareButton,
   WhatsappShareButton,
@@ -26,7 +37,11 @@ import {
 
 function SharePortfolio() {
   const [shareData, setShareData] = React.useState(window.location.href || ""); // Set your share data here
- 
+
+  useEffect(() => {
+    setShareData(window.location.href || "");
+  }, [window.location.href]);
+
   const share = [
     {
       outlet: "Whatsapp",
@@ -79,11 +94,55 @@ function SharePortfolio() {
   ];
 
   return (
-    <div className="flex gap-2 relative items-center justify-center px-8 py-1 pt-2  border border-solid border-accent rounded-full">
-      {share.map((item, index) => (
-        <span className="w-fit h-fil cursor-pointer hover:scale-105 transition-all " key={index}>{item.icon}</span>
-      ))}
-    </div>
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">Share</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-md">
+        <DialogHeader>
+          <DialogTitle>Share link</DialogTitle>
+          <DialogDescription>
+            Anyone who has this link will be able to view this.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="flex items-center space-x-2">
+          <div className="grid flex-1 gap-2">
+            <Label htmlFor="link" className="sr-only">
+              Link
+            </Label>
+            <Input id="link" defaultValue={shareData} readOnly />
+          </div>
+          <Button
+            onClick={() => {
+              navigator.clipboard.writeText(shareData);
+              toast.success("Copied to clipboard");
+            }}
+            size="sm"
+            className="px-3"
+          >
+            <span className="sr-only">Copy</span>
+            <CopyIcon className="h-4 w-4" />
+          </Button>
+        </div>
+        <div className="flex gap-2 relative items-center justify-start  py-1 pt-2  border border-solid border-accent rounded-full">
+          {share.map((item, index) => (
+            <span
+              className="w-fit h-fil cursor-pointer hover:scale-105 transition-all "
+              key={index}
+            >
+              {item.icon}
+            </span>
+          ))}
+        </div>
+        {/* <DialogFooter className="sm:justify-start">
+          <DialogClose asChild>
+            <Button type="button" variant="secondary">
+              Close
+            </Button>
+          </DialogClose>
+        </DialogFooter> */}
+      </DialogContent>
+    </Dialog>
   );
 }
 
