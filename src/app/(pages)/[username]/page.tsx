04b0b1f963b5e1f0353a8e4d790c02063afba369
aasuyadav1 <aasuy284@gmail.com>
2024-06-17 11:50:18 +0530

    fix slow data fetch

--- src/app/(pages)/[username]/page.tsx
+++ src/app/(pages)/[username]/page.tsx
@@ -1,57 +1,39 @@
-'use client'
-import React, { useEffect, useState } from 'react'
-import PortfolioAbout from '../../../components/portfolio/PortfolioAbout'
-import SkillsSection from '@/components/portfolio/PortfolioSkills'
-import PortfolioProjectCard from '@/components/portfolio/PortfolioProjectCard'
-import { useSession } from 'next-auth/react'
+"use client";
+import React, { useEffect, useState } from "react";
+import PortfolioAbout from "../../../components/portfolio/PortfolioAbout";
+import SkillsSection from "@/components/portfolio/PortfolioSkills";
+import PortfolioProjectCard from "@/components/portfolio/PortfolioProjectCard";
+import { useSession } from "next-auth/react";
+import { toast } from "sonner";
 
 const Page = ({ params }: any) => {
-  const [userid, setUserId] = useState('')
-  const [portfolioData, setPortfolioData] = useState<any>()
-  const [isLoading, setIsLoading] = useState(true)
+  const [portfolioData, setPortfolioData] = useState<any>();
+  const [isLoading, setIsLoading] = useState(true);
 
-  const fetchPortfolio = async (id: any) => {
+  const fetchPortfolio = async () => {
     try {
-      const response = await fetch('/api/portfolio/' + id, {
-        method: 'GET'
+      const response = await fetch("/api/portfolio/" + params?.username, {
+        method: "GET",
       });
 
       const data = await response.json();
 
-      if (response.ok) {
-        setPortfolioData(data.data)
-        // console.log("portfolio fetched successfully", data.data)
+      if (response.ok && data.success) {
+        setPortfolioData(data.data);
+        setIsLoading(false);
+      } else {
+        toast.error(data.message);
       }
     } catch (error) {
-      console.log("error on fetching the user ", error)
+      console.log("error on fetching the user ", error);
     } finally {
-      setIsLoading(false)
+      setIsLoading(false);
     }
-  }
-
-  const isValidDomain = async () => {
-    try {
-      const response = await fetch('/api/portfolio/domain/' + params.username, {
-        method: 'GET'
-      });
-
-      const data = await response.json();
-
-      if (response.ok) {
-        setUserId(data?.data?.userid)
-        fetchPortfolio(data.data.userid);
-        // console.log("domain fetched successfully", data.data.userid)
-      }
-    } catch (error) {
-      console.log("error on fetching the domain ", error)
-      setIsLoading(false)
-    }
-  }
+  };
 
   useEffect(() => {
-    isValidDomain();
-  }, [])
-
+    fetchPortfolio();
+  }, []);
 
   return (
     <>
@@ -61,17 +43,22 @@ const Page = ({ params }: any) => {
         </div>
       ) : (
         portfolioData && (
-          <section className='flex scroll-smooth  flex-col gap-10 py-2 px-2'>
-            <PortfolioAbout id={userid} aboutDatas={portfolioData?.about} allLinks={portfolioData?.links} />
-            <SkillsSection id={userid} skillData={portfolioData?.skills} />
-            <PortfolioProjectCard id={userid} projectData={portfolioData?.projects} />
-            {/* <PortfolioContact id={userid}/>
-            <SingleShare id={userid}/> */}
+          <section className="flex scroll-smooth  flex-col gap-10 py-2 px-2">
+            <PortfolioAbout
+              aboutDatas={portfolioData?.about}
+              allLinks={portfolioData?.links}
+            />
+            <SkillsSection skillData={portfolioData?.skills} />
+            <PortfolioProjectCard
+              projectData={portfolioData?.projects}
+            />
+            {/* <PortfolioContact/>
+            <SingleShare/> */}
           </section>
         )
       )}
     </>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
