530d1a231bde449358914cc2a64ebb27a1025627
Aasu yadav <143898193+Aasuyadav1@users.noreply.github.com>
2024-06-26 16:11:40 +0530

    Update DeleteModel.tsx

--- src/components/DeleteModel.tsx
+++ src/components/DeleteModel.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { useState } from 'react';
 import { Dialog, DialogTrigger, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
